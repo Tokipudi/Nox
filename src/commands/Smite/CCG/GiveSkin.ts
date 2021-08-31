086e2c4ad7953d730cc98aadf68d69dcb6750cc1
Tokipudi <jsd31570@gmail.com>
2021-08-31 17:33:41 +0200

    Added a command to give skins to other players.

---
+++ src/commands/Smite/CCG/GiveSkin.ts
@@ -0,0 +1,67 @@
+import { Command, CommandOptions, PieceContext } from '@sapphire/framework';
+import { Message, MessageEmbed, User } from 'discord.js';
+
+export class GiveSkin extends Command {
+    public constructor(context: PieceContext, options: CommandOptions) {
+        super(context, {
+            ...options,
+            name: 'giveskin',
+            aliases: ['give'],
+            description: 'Gives a skin you own to a user of your choice.'
+        });
+    }
+
+    public async run(message: Message, args) {
+        const user: User = await args.pick('user');
+        const skinName: string = await args.rest('string');
+
+        if (!user) return message.reply('The first argument **must** be a user.');
+        if (user.id === message.author.id) return message.reply('You cannot give yourself a skin!');
+        // if (user.bot) return message.reply('You cannot give a skin to a bot!');
+        if (!skinName) return message.reply('The second argument needs to be a valid skin name!');
+
+        skinName.trim();
+        const skin = await this.container.prisma.skins.findFirst({
+            where: {
+                name: skinName,
+                playerId: message.author.id
+            }
+        });
+        if (!skin) return message.reply('The skin **' + skinName + '** does not exist or does not belong to you!');
+
+        const player = await this.container.prisma.players.findUnique({
+            where: {
+                id: user.id
+            }
+        });
+        if (!player) {
+            await this.container.prisma.players.create({
+                data: {
+                    id: user.id,
+                    skins: {
+                        connect: {
+                            id: skin.id
+                        }
+                    }
+                }
+            });
+        } else {
+            await this.container.prisma.skins.update({
+                data: {
+                    playerId: user.id
+                },
+                select: {
+                    id: true,
+                    name: true,
+                    playerId: true
+                },
+                where: {
+                    id: skin.id
+                }
+            });
+        }
+
+        this.container.logger.info(`The skin ${skin.name}<${skin.id}> was given to ${user.username}#${user.discriminator}<${user.id}> by ${message.author.username}#${message.author.discriminator}<${message.author.id}>!`)
+        return message.reply(`The skin **${skinName}** was successfully given to ${user}!`);
+    }
+}
