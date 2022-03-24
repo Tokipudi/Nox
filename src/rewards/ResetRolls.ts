9d1327077497c73891c7bac17b1170ce1e04a365
Tokipudi <jsd31570@gmail.com>
2022-03-25 00:45:10 +0100

    Reworked the AddRoll reward to a ResetRoll.

--- src/rewards/ResetRolls.ts
+++ src/rewards/ResetRolls.ts
@@ -3,19 +3,17 @@ import { Reward } from '@lib/rewards/Reward';
 import { ApplyOptions } from '@sapphire/decorators';
 
 @ApplyOptions<RewardOptions>({
-    description: 'Get an additional roll (can go over the limit).',
+    description: 'Resets the available rolls to 3.',
     tokens: 5
 })
-export class AddRoll extends Reward {
+export class ResetRolls extends Reward {
 
     async giveReward(playerId: number): Promise<void> {
         await super.giveReward(playerId);
 
         await this.container.prisma.players.update({
             data: {
-                rollsAvailable: {
-                    increment: 1
-                },
+                rollsAvailable: 3,
                 tokens: {
                     decrement: this.tokens
                 }
